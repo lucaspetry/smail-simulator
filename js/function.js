2a3f051b2a043541384acafcb9ae06b8e6a6b811
Lucas Petry <lucasmaypetry@gmail.com>
2016-10-09 20:16:10 -0300

    Adding functions to the settings.

---
+++ js/function.js
@@ -0,0 +1,24 @@
+Function = {
+    NUMBER_OF_FUNCTIONS : 5,
+    INDEX : [
+        "CONSTANT",
+        "EXPONENCIAL",
+        "NORMAL",
+        "TRIANGULAR",
+        "UNIFORM"
+    ],
+    NUMBER : {
+        CONSTANT : 0,
+        EXPONENCIAL : 1,
+        NORMAL : 2,
+        TRIANGULAR : 3,
+        UNIFORM : 4
+    },
+    NAME : {
+        CONSTANT : "Constante",
+        EXPONENCIAL : "Exponencial",
+        NORMAL : "Normal",
+        TRIANGULAR : "Triangular",
+        UNIFORM : "Uniforme"
+    }
+};
