7828033c77b408e92e5787f9039b41dae5a9826c
Lucas May Petry <lucasmaypetry@gmail.com>
2016-10-13 15:57:04 -0300

    Settings menu updated.

--- js/smailSimulator.js
+++ js/smailSimulator.js
@@ -5,13 +5,14 @@ function Simulation() {
     "use strict";
     this.simulationTime = 500;
     this.simulationSeed = 11;
+    this.simulationSpeed = 5;
     
     // Volume de tráfego
     this.traffic = [0, 0, 0, 0];
     this.traffic[Direction.NUMBER.LL] = 50;
     this.traffic[Direction.NUMBER.LR] = 25;
     this.traffic[Direction.NUMBER.RL] = 15;
-    this.traffic[Direction.NUMBER.RR] = 5;
+    this.traffic[Direction.NUMBER.RR] = 10;
     
     // Taxas de sucesso, falha e adiamento, respectivamente
     // Deve totalizar 100%
@@ -21,20 +22,27 @@ function Simulation() {
     this.trafficRate[Direction.NUMBER.RL] = [96.0, 3.0,  1.0];
     this.trafficRate[Direction.NUMBER.RR] = [96.0, 1.0,  9.0];
     
+    // Tempo de recepção de acordo com a direção
+    this.receptionTime = [0, 0, 0, 0];
+    this.receptionTime[Direction.NUMBER.LL] = 0.12;
+    this.receptionTime[Direction.NUMBER.LR] = 0.12;
+    this.receptionTime[Direction.NUMBER.RL] = 0.14;
+    this.receptionTime[Direction.NUMBER.RR] = 0.16;
+    
     // Processamento das requisições
-    this.processing = {
-        LL : [[0.12, Function.NUMBER.NORMAL, 0.55, 0.05],
-              [0.14, Function.NUMBER.TRIANGULAR, 0.02, 0.05, 0.12],
-              [0.11, Function.NUMBER.UNIFORM, 0.06, 0.15]],
-        LR : [[0.12, Function.NUMBER.NORMAL, 0.65, 0.04],
-              [0.13, Function.NUMBER.UNIFORM, 0.16, 0.25],
-              [0.15, Function.NUMBER.TRIANGULAR, 0.05, 0.07, 0.10]],
-        RL : [[0.12, Function.NUMBER.UNIFORM, 0.03, 0.11],
-              [0.14, Function.NUMBER.NORMAL, 0.46, 0.05],
-              [0.11, Function.NUMBER.NORMAL, 0.72, 0.09]],
-        RR : [[0.16, Function.NUMBER.UNIFORM, 0.09, 0.18],
-              [0.13, Function.NUMBER.TRIANGULAR, 0.08, 0.15, 0.22],
-              [0.16, Function.NUMBER.NORMAL, 0.63, 0.04]]
+    this.serviceTime = {
+        LL : [[Function.NUMBER.NORMAL, 0.55, 0.05, 0],
+              [Function.NUMBER.TRIANGULAR, 0.02, 0.05, 0.12],
+              [Function.NUMBER.UNIFORM, 0.06, 0.15, 0]],
+        LR : [[Function.NUMBER.NORMAL, 0.65, 0.04, 0],
+              [Function.NUMBER.UNIFORM, 0.16, 0.25, 0],
+              [Function.NUMBER.TRIANGULAR, 0.05, 0.07, 0.10]],
+        RL : [[Function.NUMBER.UNIFORM, 0.03, 0.11, 0],
+              [Function.NUMBER.NORMAL, 0.46, 0.05, 0],
+              [Function.NUMBER.NORMAL, 0.72, 0.09, 0]],
+        RR : [[Function.NUMBER.UNIFORM, 0.09, 0.18, 0],
+              [Function.NUMBER.TRIANGULAR, 0.08, 0.15, 0.22],
+              [Function.NUMBER.NORMAL, 0.63, 0.04, 0]]
     };
 }
 
